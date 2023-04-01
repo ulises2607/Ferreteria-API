86fcb0adc30bd1925746d8ce00d68f5572d58fce
ulises2607 <ulisescappa@gmail.com>
2023-04-01 19:56:42 -0300

    primero

---
+++ src/main/java/com/oga/ferreteria/api/FerreteriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.oga.ferreteria.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oga/ferreteria/api/FerreteriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oga.ferreteria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
