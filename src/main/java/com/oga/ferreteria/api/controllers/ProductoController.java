2e5882349c2c45870c2bbd30bba12bf945ac3ab0
ulises2607 <ulisescappa@gmail.com>
2023-04-08 21:11:26 -0300

    Agregando Cors

--- src/main/java/com/oga/ferreteria/api/controllers/ProductoController.java
+++ src/main/java/com/oga/ferreteria/api/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package com.oga.ferreteria.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.oga.ferreteria.api.modelos.entity.Producto;
 import com.oga.ferreteria.api.modelos.services.IProductoService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/producto")
 public class ProductoController {
